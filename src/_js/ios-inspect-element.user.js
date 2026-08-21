986da10d96d7ede88be873d7a259e770f9e62b84
steventheworker <steventheworker@gmail.com>
2026-08-21 15:19:39 -0700

    ios-inspect-element (don't autoload eruda)

--- src/_js/ios-inspect-element.user.js
+++ src/_js/ios-inspect-element.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ios-inspect-element
 // @namespace
-// @version      0.0.1
+// @version      0.0.7
 // @description  inject console (eruda) on-demand
 // @author       steventheworker
 // @match        *://*/*
@@ -10,17 +10,57 @@
 // @license      WTFPL
 // ==/UserScript==
 
-(function () {
-	const script = document.createElement("script");
+let erudaActive = false;
 
+function toggleInspectElementElement() {
+	if (document.querySelector("#toggleInspectElementElement")) {
+		// erudaActive is true when Eruda is currently open, so hide it first
+		eruda[erudaActive ? "hide" : "show"]();
+		erudaActive = !erudaActive;
+		return;
+	}
+
+	erudaActive = true;
+	const script = document.createElement("script");
+	script.id = "toggleInspectElementElement";
 	script.src = "//cdn.jsdelivr.net/npm/eruda";
 	script.async = true;
 	script.onload = () => {
 		const initScript = document.createElement("script");
-		initScript.textContent = "eruda.init();";
+		initScript.textContent = `
+          eruda.init({
+             defaults: {
+                displaySize: 80,
+                transparency: 95
+             }
+          });
+          eruda.show();
+          if (eruda._entryBtn) eruda._entryBtn.hide();
+       `;
 		document.body.appendChild(initScript);
 	};
 	document.body.appendChild(script);
+	if (typeof completion === "function") completion(location.href);
+}
 
-	completion(location.href);
+(function () {
+	if (typeof window.$isSafari === "function" && !window.$isSafari()) return;
+
+	window.addEventListener("keydown", (e) => {
+		if (
+			(e.key === "`" && e.metaKey) ||
+			(e.key === "i" && e.metaKey && e.altKey)
+		) {
+			const w0 = document.body.clientWidth;
+			const h0 = document.body.clientHeight;
+			setTimeout(() => {
+				if (
+					document.body.clientWidth === w0 &&
+					document.body.clientHeight === h0
+				) {
+					toggleInspectElementElement();
+				}
+			}, 333);
+		}
+	});
 })();
